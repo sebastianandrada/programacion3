fe8ee659e856f5cbda4705683ab58e3c4d61f36c
sebastian.andrada <sebastian.andrada@icbc.com.ar>
2019-08-26 10:16:14 -0300

    programacion 3

--- clase01/funciones.js
+++
@@ -1,9 +0,0 @@
-function mostrar() {
-    var usr = document.getElementById("usr").value
-    var pass = document.getElementById("pass").value
-    if (usr === "asd" && pass === "1234") {
-      alert('todo esta ok')
-    } else {
-      alert('usuario y/o password incorrectos')
-    }
-  }
